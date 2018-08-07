84268c3498a987c0772ea67961cade32be3f8f04
Jerome de Boeck <jerome_de_boeck@hotmail.com>
2018-08-07 11:44:10 +0100

    add user key to review

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_07_102923) do
+ActiveRecord::Schema.define(version: 2018_08_07_104325) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,8 @@ ActiveRecord::Schema.define(version: 2018_08_07_102923) do
   create_table "reviews", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -78,6 +80,7 @@ ActiveRecord::Schema.define(version: 2018_08_07_102923) do
   add_foreign_key "coats", "categories"
   add_foreign_key "coats", "reviews"
   add_foreign_key "coats", "users"
+  add_foreign_key "reviews", "users"
   add_foreign_key "users", "coats"
   add_foreign_key "users", "reviews"
 end
