2763bd35ca7c049aac7797140c9f644e8cfa8c7d
Tom Gow-Gates <tomrgg@gmail.com>
2018-08-07 09:34:22 +0100

    ran db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_06_165155) do
+ActiveRecord::Schema.define(version: 2018_08_07_081759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 2018_08_06_165155) do
   create_table "bookings", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "coat_id"
+    t.bigint "user_id"
+    t.date "start_date"
+    t.date "end_date"
+    t.index ["coat_id"], name: "index_bookings_on_coat_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -52,4 +58,6 @@ ActiveRecord::Schema.define(version: 2018_08_06_165155) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "coats"
+  add_foreign_key "bookings", "users"
 end
