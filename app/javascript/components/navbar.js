60f1471a6233d310f252f749f43c0ec737422d25
Jerome de Boeck <jerome_de_boeck@hotmail.com>
2018-08-09 23:27:08 +0100

    productive evening for jj

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -2,7 +2,7 @@ function initUpdateNavbarOnScroll() {
   const navbar = document.querySelector('.navbar-wagon');
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight) {
+      if (window.scrollY) {
         navbar.classList.add('navbar-wagon-blue');
       } else {
         navbar.classList.remove('navbar-wagon-blue');
