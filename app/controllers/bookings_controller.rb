f1a45f146bf3c0c7f5001e59b30b6de69e8d0d1a
Tom Gow-Gates <tomrgg@gmail.com>
2018-08-09 17:18:09 +0100

    fixed

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -22,8 +22,8 @@ class BookingsController < ApplicationController
   end
 
   def show
-    @booking = Booking.find(params[:id])
-    @coat = Coat.find(params[:coat_id])
+    @user  = current_user
+    @bookings = Booking.where(user: @user)
   end
 
   private
